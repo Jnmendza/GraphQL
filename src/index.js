1633772eefb6a74381ca19af3e84d65160a1ce6a
Jnmendza <jonatmendoza@gmail.com>
2020-08-04 19:01:13 -0700

    relationships both directions

--- src/index.js
+++ src/index.js
@@ -25,27 +25,56 @@ const posts = [{
     title: 'GraphQl',
     body: 'GraphQL body goes here',
     published: true,
-    author: '1'
+    author: '1',
+    comment: '098'
 },
 {
     id: '322',
     title: 'Fundamentals',
     body: 'This is the body for fundamentals',
     published: false,
-    author: '1'
+    author: '1',
+    comment: '096'
 },
 {
     id: '323',
     title: 'Basics',
     body: 'This is the body for basics',
     published: true,
-    author: '2'
+    author: '2',
+    comment: '094'
 },]
+
+const comments = [{
+    id: '098',
+    text: 'I loved the course keep up the good work!',
+    author: '2',
+    post: '323'
+},
+{
+    id: '096',
+    text: 'Some other comments you have been waiting for',
+    author: '3',
+    post: '321'
+},
+{
+    id: '094',
+    text: 'More comments because why not',
+    author: '1',
+    post: '322'
+},
+{
+    id: '092',
+    text: 'Essential comments goes here',
+    author: '2',
+    post: '322'
+}]
 
 const typeDefs = `
     type Query {
         users(query: String): [User!]!
         posts(query: String): [Post!]!
+        comments: [Comment!]!
         me: User!
         post: Post!
     }
@@ -55,6 +84,8 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
+        comments: [Comment!]!
     }
 
     type Post {
@@ -63,6 +94,14 @@ const typeDefs = `
         body: String!
         published: Boolean!
         author: User!
+        comments: [Comment!]!
+    }
+
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
+        post: Post!
     }
 `
 
@@ -90,6 +129,9 @@ const resolvers = {
                 return isTitleMatch || isBodyMatch
             })
         },
+        comments(parent, args, ctx, info) {
+            return comments
+        },
         me() {
             return{
                 id: '123098',
@@ -111,6 +153,35 @@ const resolvers = {
         author(parent, args, ctx, info){
             return users.find((user) => {
                 return user.id === parent.author
+            })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.post === parent.id
+            })
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info) {
+            return posts.filter((post) => {
+                return post.author === parent.id
+            })
+        },
+        comments(parent, args, ctx ,info) {
+            return comments.filter((comment) => {
+                return comment.author === parent.id
+            })
+        }
+    },
+    Comment: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        },
+        post(parent, args, ctx, info) {
+            return posts.find((post) => {
+                return post.id === parent.post
             })
         }
     }
