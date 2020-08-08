a72a8fd8c7c08f96283c69a1fe967016c9f8164c
Jnmendza <jonatmendoza@gmail.com>
2020-08-07 22:46:12 -0700

    spread op

--- src/index.js
+++ src/index.js
@@ -168,9 +168,7 @@ const resolvers = {
             
             const user = {
                 id: uuid(),
-                name: args.name,
-                email: args. email,
-                age: args.age
+                ...args
             }
 
             users.push(user)
@@ -189,10 +187,7 @@ const resolvers = {
 
             const post = {
                 id: uuid(),
-                title: args.title,
-                body: args.body,
-                published: args.published,
-                author: args.author
+                ...args
             }
 
             posts.push(post)
@@ -215,9 +210,7 @@ const resolvers = {
 
             const comment = {
                 id: uuid(),
-                text: args.text,
-                author: args.author,
-                post: args.post
+                ...args
             }
 
             comments.push(comment)
