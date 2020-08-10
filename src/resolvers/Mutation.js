8ebbd147eef2700c989f2ff768d523593038d32f
Jnmendza <jonatmendoza@gmail.com>
2020-08-10 09:39:44 -0700

    update user mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -42,6 +42,34 @@ const Mutation = {
 
         return deletedUsers
     },
+    updateUser(parent, args , { db }, info) {
+        const { id, data } = args
+        const user = db.users.find((user) => user.id === id)
+
+        if (!user) {
+            throw new Error("User not found")
+        }
+
+        if (typeof data.email === 'string') {
+            const emailTaken = db.users.some((user) => user.email === data.email)
+
+            if (emailTaken) {
+                throw new Error("Email taken")
+            }
+
+            user.email = data.email
+        }
+
+        if (typeof data.name === 'string') {
+            user.name = data.name
+        }
+
+        if (typeof data.age !== 'undefined') {
+            user.age = data.age
+        }
+
+        return user
+    },
     createPost(parent, args, { db }, info) {
         // make sure the author id given matches the id for one of the users
         const userExist = db.users.some((user) => {
