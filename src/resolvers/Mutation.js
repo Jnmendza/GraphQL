241712cedb5e440ae87d9107edbcb2a5a84d5198
Jnmendza <jonatmendoza@gmail.com>
2020-08-10 10:29:33 -0700

    update post and comment

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,6 +1,5 @@
 import {uuid} from 'uuidv4'
 
-
 const Mutation = {
     createUser(parent, args, { db }, info) {
         const emailTaken = db.users.some((user) => {
@@ -104,6 +103,29 @@ const Mutation = {
         return deletedPosts[0]
 
     },
+    updatePost(parent, args, { db }, info) {
+        const { id, data } = args
+        const post = db.posts.find((post) => post.id === id)
+
+        if (!post) {
+            throw new Error("Post does not exist")
+        }
+
+        if (typeof data.title === 'string') {
+            post.title = data.title
+        }
+
+        if (typeof data.body === 'string') {
+            post.body = data.body
+        }
+
+        if (typeof data.published === 'boolean') {
+            post.published = data.published
+        }
+
+        return post
+
+    },
     createComment(parent, args, { db }, info) {
         const userExist = db.users.some((user) => {
             return user.id === args.comments.author
@@ -136,6 +158,20 @@ const Mutation = {
         const deletedComments = db.comments.splice(commentIndex, 1)
 
         return deletedComments[0]
+    },
+    updateComment(parent, args, { db }, info) {
+        const { id, data } = args
+        const comment = db.comments.find((comment) => comment.id === id)
+
+        if (!comment) {
+            throw new Error("Comment does not exist")
+        }
+
+        if (typeof data.text === 'string') {
+            comment.text = data.text
+        }
+
+        return comment
     }
 }
 
